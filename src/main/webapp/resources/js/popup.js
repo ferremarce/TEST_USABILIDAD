1985d884e8055970283c46500b56b9b83c3149da
jmferreira1978 <jmferreira1978@gmail.com>
2015-09-24 10:38:23 -0400

    - Se agregan los popus a todas las tareas y al pasar a la siguiente tarea se cierra automaticamente la ventana emergente

---
+++ src/main/webapp/resources/js/popup.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var myWindow;
+function openWin(url) {
+    myWindow = window.open(url, "myWindow", "width=400, height=200");
+    var w = screen.width - 50;
+    var h = screen.height - 100;
+    var left = (screen.width / 2) - (w / 2);
+    var top = (screen.height / 2) - (h / 2);
+    myWindow.moveTo(top, left);
+    myWindow.resizeTo(w, h);
+}
+function closeWin() {
+    myWindow.close();
+}
+
+
